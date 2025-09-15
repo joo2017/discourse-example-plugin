37d2a67a086dc325c11115289b0192b0a61afe9d
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-15 21:57:24 +0900

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# name: discourse-example-plugin
+# about: 一个示例插件，展示如何创建工具栏按钮和模态框
+# version: 0.1.0
+# authors: Your Name
+# url: https://github.com/your-username/discourse-example-plugin
+
+enabled_site_setting :example_plugin_enabled
+
+register_asset "stylesheets/example-plugin.scss"
+
+after_initialize do
+  # 服务器端初始化代码（如需要）
+end
