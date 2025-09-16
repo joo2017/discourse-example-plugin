b89d25f1f787b56627b5fc6d433d3d00f4287085
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-16 20:42:35 +0900

    Update example-plugin-init.js

--- assets/javascripts/discourse/initializers/example-plugin-init.js
+++ assets/javascripts/discourse/initializers/example-plugin-init.js
@@ -1,53 +1,38 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
-import { service } from "@ember/service";
-
-function initializeExamplePlugin(api) {
-  api.onToolbarCreate((toolbar) => {
-    toolbar.addButton({
-      id: "example-plugin-button",
-      group: "extras",
-      icon: "magic",
-      title: "example_plugin.toolbar_button.title",
-      sendAction: (event) => toolbar.context.send("showExampleModal", event),
-    });
-  });
-
-  // 修改 d-editor 组件来处理模态框
-  api.modifyClass("component:d-editor", {
-    modal: service(),
-    pluginId: "discourse-example-plugin",
-    
-    actions: {
-      showExampleModal(toolbarEvent) {
-        console.log("Showing modal with toolbar event:", toolbarEvent);
-        
-        import("../components/example-modal").then((module) => {
-          const ExampleModal = module.default;
-          
-          this.modal.show(ExampleModal, {
-            model: {
-              toolbarEvent: toolbarEvent,
-              insertText: (text) => {
-                if (toolbarEvent && toolbarEvent.addText) {
-                  toolbarEvent.addText(text);
-                } else {
-                  // 备用方案：直接操作编辑器
-                  this.appEvents.trigger("composer:insert-text", text);
-                }
-              }
-            }
-          });
-        }).catch((error) => {
-          console.error("Failed to load modal component:", error);
-        });
-      }
-    }
-  });
-}
 
 export default {
   name: "example-plugin-init",
   initialize() {
-    withPluginApi("0.12.0", initializeExamplePlugin);
+    withPluginApi("1.0.0", (api) => {
+      api.onToolbarCreate((toolbar) => {
+        toolbar.addButton({
+          id: "example-plugin-button",
+          group: "extras",
+          icon: "magic",
+          title: "example_plugin.toolbar_button.title",
+          perform: () => {
+            const modal = api.container.lookup("service:modal");
+            
+            // 使用相对路径导入
+            import("../components/modal/example-modal").then((module) => {
+              const ExampleModal = module.default;
+              modal.show(ExampleModal, {
+                model: {
+                  insertText: (text) => {
+                    // 获取 composer 控制器来插入文本
+                    const composer = api.container.lookup("controller:composer");
+                    if (composer && composer.model) {
+                      composer.model.appendText(text);
+                    }
+                  }
+                }
+              });
+            }).catch((error) => {
+              console.error("Failed to load modal component:", error);
+            });
+          }
+        });
+      });
+    });
   },
 };
